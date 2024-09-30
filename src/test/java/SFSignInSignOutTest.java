3ab39f0ea78ac39fa5173df580b3d44e49d8c880
Taras Oleksyn <taras.oleksyn@gmail.com>
2024-09-30 14:52:53 +0300

    Update SFSignInSignOutTest.java

--- src/test/java/SFSignInSignOutTest.java
+++ src/test/java/SFSignInSignOutTest.java
@@ -1,17 +1,19 @@
 import org.testng.annotations.Test;
+
 public class SFSignInSignOutTest {
 
     @Test
     public void verifySignInWorks() {
-        var homePage = new SFHomePage().open();
-        homePage.closeExtra15();
-        SFSignInPage signInPage = homePage.signInDropdownOpen().signInClick();
-        signInPage.signIn("testsf@g.com", "12345678s");
+        var homePage = new SFHomePage()
+            .open()
+            .closeExtraPopup();
 
-        // Verify that user is signed in
-        AccountPage accountPage = homePage.signInDropdownOpen().openAccountSettingsPage();
-        accountPage.verifyUserSignedInAs("testsf@g.com");
-
+        var username = "testsf@g.com";
+        homePage.signIn(username, "12345678s"); // openDropDown().signIn();
+        
+        logginInPage
+            .openAccountSettingsPage(); // .signInDropdownOpen() 
+            .verifyUserSignedInAs(username);
     }
 
     @Test
