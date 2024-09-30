31b2302d9d2bc8d8fb9b2d1b2274a0e7889a784e
Serhii Nesterov <nesterov@testfort.com>
2024-09-30 13:31:45 +0300

    First commit

--- src/main/java/AccountPage.java
+++
@@ -1,9 +0,0 @@
-import com.codeborne.selenide.Selenide;
-import static com.codeborne.selenide.Condition.text;
-
-public class AccountPage {
-
-    public void verifyUserSignedInAs(String email) {
-        Selenide.$(".contact-info-section").shouldHave(text(email));
-    }
-}
